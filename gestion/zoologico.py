06074957ec446c590241deac78ff0e10402b8923
SebastianHoyoss <jhoyosc@unal.edu.co>
2023-09-25 15:48:11 -0500

    Update zoologico.py

---
+++ gestion/zoologico.py
@@ -0,0 +1,28 @@
+from gestion.zona import Zona
+class Zoologico:
+    _zonas =[]
+
+    def __init__(self,nombre,ubicacion):
+        self._nombre=nombre
+        self._ubicacion=ubicacion
+    def agregarZonas(self,zona):
+        self._zonas.append(zona)
+    def cantidadTotalAnimales(self):
+        cont=0
+        for zona in self._zonas:
+            cont+=zona.cantidadAnimales()
+        return cont
+    def getNombre(self):
+        return self._nombre
+    def setNombre(self,nombre):
+        self._nombre=nombre
+    def getUbicacion(self):
+        return self._ubicacion
+    def setUbicacion(self,ubicacion):
+        self._ubicacion=ubicacion
+    @classmethod
+    def getZona(cls):
+        return cls._zonas
+    @classmethod
+    def setZona(cls,zonas):
+        cls._zonas=zonas
